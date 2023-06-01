7de3703ad7714c962f59442a02027f7f980c8ea2
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2023-06-01 19:49:10 +0900

    글쓰기 개선

--- src/components/views/BoardWrite/BoardWrite.js
+++ src/components/views/BoardWrite/BoardWrite.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import '../BoardWrite/BoardWrite.css'
@@ -34,56 +34,176 @@ const BoardWrite = () => {
     navigate('/board/worry')
   }
 
+  const categoryMoreRef = useRef(null)
+  const writeCategoryRef = useRef(null)
+
+  const [isActive, setIsActive] = useState(false)
+
+  const categoryClickHandler = () => {
+    setIsActive((current) => !current)
+  }
+  const handleItemClick = (text) => {
+    const categoryMore = categoryMoreRef.current
+    const writeCategory = writeCategoryRef.current
+    categoryMore.classList.remove('active')
+    writeCategory.textContent = text
+    setIsActive(false)
+  }
+
   return (
     <>
       <div className="content">
         <div className="write__wrap">
-          <div className="write__header">
-            <button className="write__category">일반 고민</button>
-            <div className="category_more">
-              <h4 className="category_title">고민사연</h4>
-              <ul className="category_group">
-                <li className="category__item">일반 고민</li>
-                <li className="category__item">취업/진로</li>
-                <li className="category__item">직장</li>
-                <li className="category__item">연애</li>
-                <li className="category__item">성추행</li>
-                <li className="category__item">결혼/육아</li>
-                <li className="category__item">대인관계</li>
-                <li className="category__item">외모</li>
-                <li className="category__item">가족</li>
-                <li className="category__item">학업/고시</li>
-                <li className="category__item">금전/사업</li>
-                <li className="category__item">성생활</li>
-                <li className="category__item">이별/이혼</li>
-                <li className="category__item">중독/집착</li>
-                <li className="category__item">투병/신체</li>
-                <li className="category__item">정신건강</li>
-                <li className="category__item">LGBT</li>
-                <li className="category__item">자아/성격</li>
-                <li className="category__item">따돌림</li>
-              </ul>
-              <h4 className="category_title">응원 사연</h4>
-              <ul className="category_group">
-                <li className="category__item">응원</li>
-              </ul>
-              <h4 className="category_title">자유 사연</h4>
-              <ul className="category_group">
-                <li className="category__item">자유</li>
-              </ul>
-            </div>
-            <div className="write__opt">
-              <label htmlFor="opt_input1">
-                <input
-                  type="checkbox"
-                  className="opt_input"
-                  name="opt_input"
-                  id="opt_input1"
-                />
-                <span className="opt_txt">닉네임 비공개</span>
-              </label>
-            </div>
+          <button
+            ref={writeCategoryRef}
+            className="write__category"
+            onClick={categoryClickHandler}
+          >
+            일반 고민
+          </button>
+          <div className="write__opt">
+            <label htmlFor="opt_input1">
+              <input
+                type="checkbox"
+                className="opt_input"
+                name="opt_input"
+                id="opt_input1"
+              />
+              <span className="opt_txt">닉네임 비공개</span>
+            </label>
           </div>
+
+          <div
+            ref={categoryMoreRef}
+            className={`category_more  ${isActive ? 'active' : ''}`}
+          >
+            <h4 className="category_title">고민사연</h4>
+            <ul className="category_group">
+              <li
+                className="category__item"
+                onClick={() => handleItemClick('일반 고민')}
+              >
+                일반 고민
+              </li>
+              <li
+                className="category__item"
+                onClick={() => handleItemClick('취업/진로')}
+              >
+                취업/진로
+              </li>
+              <li
+                className="category__item"
+                onClick={() => handleItemClick('직장')}
+              >
+                직장
+              </li>
+              <li
+                className="category__item"
+                onClick={() => handleItemClick('연애')}
+              >
+                연애
+              </li>
+              <li
+                className="category__item"
+                onClick={() => handleItemClick('성추행')}
+              >
+                성추행
+              </li>
+              <li
+                className="category__item"
+                onClick={() => handleItemClick('결혼/육아')}
+              >
+                결혼/육아
+              </li>
+              <li
+                className="category__item"
+                onClick={() => handleItemClick('대인관계')}
+              >
+                대인관계
+              </li>
+              <li
+                className="category__item"
+                onClick={() => handleItemClick('외모')}
+              >
+                외모
+              </li>
+              <li
+                className="category__item"
+                onClick={() => handleItemClick('가족')}
+              >
+                가족
+              </li>
+              <li
+                className="category__item"
+                onClick={() => handleItemClick('학업/고시')}
+              >
+                학업/고시
+              </li>
+              <li
+                className="category__item"
+                onClick={() => handleItemClick('금전/사업')}
+              >
+                금전/사업
+              </li>
+              <li
+                className="category__item"
+                onClick={() => handleItemClick('성생활')}
+              >
+                성생활
+              </li>
+              <li
+                className="category__item"
+                onClick={() => handleItemClick('이별/이혼')}
+              >
+                이별/이혼
+              </li>
+              <li
+                className="category__item"
+                onClick={() => handleItemClick('중독/집착')}
+              >
+                중독/집착
+              </li>
+              <li
+                className="category__item"
+                onClick={() => handleItemClick('투병/신체')}
+              >
+                투병/신체
+              </li>
+              <li
+                className="category__item"
+                onClick={() => handleItemClick('정신건강')}
+              >
+                정신건강
+              </li>
+              <li
+                className="category__item"
+                onClick={() => handleItemClick('LGBT')}
+              >
+                LGBT
+              </li>
+              <li
+                className="category__item"
+                onClick={() => handleItemClick('자아/성격')}
+              >
+                자아/성격
+              </li>
+              <li
+                className="category__item"
+                onClick={() => handleItemClick('따돌림')}
+              >
+                따돌림
+              </li>
+            </ul>
+            <h4 className="category_title">응원 사연</h4>
+            <ul className="category_group">
+              <li className="category__item">응원</li>
+            </ul>
+            <h4 className="category_title">자유 사연</h4>
+            <ul className="category_group">
+              <li className="category__item">자유</li>
+            </ul>
+          </div>
+
           <div className="write__desc">
             <input
               type="text"
