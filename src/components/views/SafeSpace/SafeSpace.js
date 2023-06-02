0fe233cfd34782c788445a5a1d4b582960245251
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2023-06-02 11:14:20 +0900

    채팅방 개선

--- src/components/views/SafeSpace/SafeSpace.js
+++ src/components/views/SafeSpace/SafeSpace.js
@@ -1,53 +1,77 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import '../SafeSpace/SafeSpace.css'
 
-function SafeSpace() {
+const SafeSpace = () => {
+  const [messages, setMessages] = useState([])
+
+  const messageListRef = useRef(null)
+
+  useEffect(() => {
+    scrollToBottom()
+  }, [messages])
+
+  const scrollToBottom = () => {
+    messageListRef.current.scrollTop = messageListRef.current.scrollHeight
+  }
+
+  const sendMessage = (message, itsMe) => {
+    setMessages((prevMessages) => {
+      const lastMessage = prevMessages[prevMessages.length - 1]
+
+      if (lastMessage && lastMessage.itsMe === itsMe) {
+        lastMessage.messages.push(message)
+        return [...prevMessages]
+      } else {
+        return [
+          ...prevMessages,
+          {
+            itsMe,
+            messages: [message],
+          },
+        ]
+      }
+    })
+  }
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter' && event.target.value.trim() !== '') {
+      sendMessage(event.target.value, true)
+      event.target.value = ''
+    }
+  }
+
+  const renderMessages = () => {
+    return messages.map((messageGroup, index) => {
+      const { itsMe, messages } = messageGroup
+      const className = itsMe ? 'me' : 'not-me'
+
+      return (
+        <div key={index} className={className}>
+          {messages.map((message, index) => (
+            <span className="block" key={index}>
+              {message}
+            </span>
+          ))}
+        </div>
+      )
+    })
+  }
+
   return (
-    <>
-      <section class="chatbox">
-        <section class="chat-window">
-          <article class="msg-container msg-remote" id="msg-0">
-            <div class="msg-box">
-              <img class="user-img" id="user-0" alt="user-img" />
-              <div class="flr">
-                <div class="messages">
-                  <p class="msg" id="msg-0">
-                    메세지 내용
-                  </p>
-                </div>
-                <span class="timestamp">
-                  <span class="username">사용자 이름</span>&bull;
-                  <span class="posttime">등록 시간</span>
-                </span>
-              </div>
-            </div>
-          </article>
-          <article class="msg-container msg-self" id="msg-0">
-            <div class="msg-box">
-              <div class="flr">
-                <div class="messages">
-                  <p class="msg" id="msg-1">
-                    Lorem ipsum dolor sit amet
-                  </p>
-                  <p class="msg" id="msg-2">
-                    Praesent varius
-                  </p>
-                </div>
-                <span class="timestamp">
-                  <span class="username">Name</span>&bull;
-                  <span class="posttime">2 minutes ago</span>
-                </span>
-              </div>
-              <img class="user-img" id="user-0" alt="user-img" />
-            </div>
-          </article>
-        </section>
-        <form class="chat-input" onsubmit="return false;">
-          <input type="text" autocomplete="on" placeholder="Type a message" />
-          <button>보내기</button>
-        </form>
-      </section>
-    </>
+    <div className="chat-box">
+      <header>
+        <h1>Chat</h1>
+      </header>
+      <section ref={messageListRef}>{renderMessages()}</section>
+      <footer>
+        <input
+          type="text"
+          id="message-input"
+          placeholder="Type a message..."
+          onKeyPress={handleKeyPress}
+        />
+      </footer>
+    </div>
   )
 }
 
