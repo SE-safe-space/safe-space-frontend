1dcacc0a71e195571f9e59b1c26a605afbe30a93
Hyewon <dudmin335@naver.com>
2023-06-06 01:11:17 +0900

    commit

--- src/components/views/CounselorList/CounselorList.js
+++ src/components/views/CounselorList/CounselorList.js
@@ -2,16 +2,16 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import '../CounselorList/CounselorList.css'
 import { Link } from 'react-router-dom'
-
-
+//import { useSelector } from 'react-redux'
 
 const CounselorList = () => {
   const [counselors, setCounselors] = useState([]);
+  //const { accessToken } = useSelector((state) => state.authToken)
 
   useEffect(() => {
     const fetchCounselors = async () => {
       try {
-        const response = await axios.get('https://port-0-safe-space-backend-otjl2cli2ssvyo.sel4.cloudtype.app/safe/consult/counselor'); // Replace with your API endpoint URL
+        const response = await axios.get('https://port-0-safe-space-backend-otjl2cli2ssvyo.sel4.cloudtype.app/safe/consult/counselor');
         setCounselors(response.data);
       } catch (error) {
         console.error('Error fetching counselors:', error);
@@ -21,6 +21,24 @@ const CounselorList = () => {
     fetchCounselors();
   }, []);
 
+  /*useEffect(() => {
+    const fetchCounselors = async () => {
+      try {
+        const response = await axios.get('https://port-0-safe-space-backend-otjl2cli2ssvyo.sel4.cloudtype.app/safe/consult/counselor',
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          }
+        });
+        setCounselors(response.data);
+      } catch (error) {
+        console.error('Error fetching counselors:', error);
+      }
+    };
+
+    fetchCounselors();
+  }, []);*/
+
   return (
       <div className="content_wrap">
         <div className="counselor_list_header">
@@ -29,7 +47,7 @@ const CounselorList = () => {
         <ul className="counselor_list">
           {counselors.map((counselor) => (
             <li className="counselor_profile" key={counselor.id}>
-              <Link to={`/counselor/id`}>
+              <Link to={`/counselor/${counselor.id}`}>
                 <div className="profile_img">
                   <img src={counselor.profileImage} alt="profile" />
                 </div>
