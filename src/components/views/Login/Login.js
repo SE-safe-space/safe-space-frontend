ae229b3940f070b304df2058950bd95ce48149e8
Hyewon <dudmin335@naver.com>
2023-06-11 03:14:39 +0900

    예약 기능 구현

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -16,36 +16,12 @@ const Input = tw.input`
   appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm
 `
 
-function Login() {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-
-  // useForm 사용을 위한 선언
-  const {
-    register,
-    setValue,
-    formState: { errors },
-    handleSubmit,
-  } = useForm()
-
-  // submit 이후 동작할 코드
-  // 백으로 유저 정보 전달
-  const onValid = async ({ email, password }) => {
-    const response = await loginUser({ email, password })
-
-    if (response.status) {
-      // 쿠키에 Refresh Token, store에 Access Token 저장
-      setRefreshToken(response.json.refreshToken)
-      dispatch(SET_TOKEN(response.json.accessToken))
-      console.log(response.json)
-
-      return navigate('/')
-    } else {
-      console.log(response.json)
-    }
-    // input 태그 값 비워주는 코드
-    setValue('password', '')
-  }
+const Login = ({ onLogin }) => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { email, password } = event.target.elements;
+    onLogin(email.value, password.value);
+  };
 
   return (
     <div className="content">
@@ -53,7 +29,7 @@ function Login() {
         <div className="max-w-md w-full space-y-8">
           <form
             className="mt-8 space-y-6 login-form"
-            onSubmit={handleSubmit(onValid)}
+            onSubmit={handleSubmit}
           >
             <input type="hidden" name="remember" defaultValue="true" />
             <div className="rounded-md shadow-sm -space-y-px">
